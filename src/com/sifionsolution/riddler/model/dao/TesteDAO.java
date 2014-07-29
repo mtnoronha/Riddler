85ff996486b690f4a392598b50c3511aef2c300b
Marco <mtnoronha7@gmail.com>
2014-07-29 10:38:53 -0300

    Need to use @Inject to inject EntityManager

--- src/com/sifionsolution/riddler/model/dao/TesteDAO.java
+++ src/com/sifionsolution/riddler/model/dao/TesteDAO.java
@@ -1,15 +1,15 @@
 package com.sifionsolution.riddler.model.dao;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import com.sifionsolution.riddler.model.Teste;
 
 @RequestScoped
 public class TesteDAO {
 
-	@PersistenceContext
+	@Inject
 	private EntityManager manager;
 
 	public void save(Teste obj) {
