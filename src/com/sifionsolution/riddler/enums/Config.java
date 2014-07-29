8a9ac09e6cc2598803e85f2ca5b9a3d0bea8e861
Marco <mtnoronha7@gmail.com>
2014-07-29 11:26:00 -0300

    Configuration enum to help out with the location with some properties file

---
+++ src/com/sifionsolution/riddler/enums/Config.java
@@ -0,0 +1,18 @@
+package com.sifionsolution.riddler.enums;
+
+import static java.lang.String.format;
+
+public enum Config {
+	DATABASE("database.properties");
+
+	private static final String ROOT = "/config/%s";
+	private String file;
+
+	private Config(String file) {
+		this.file = file;
+	}
+
+	public String getPath() {
+		return format(ROOT, file);
+	}
+}
