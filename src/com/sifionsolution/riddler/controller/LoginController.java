c07ef58af8ad447ab00097034f5ec344fd78e532
Marco <mtnoronha7@gmail.com>
2014-07-30 11:02:45 -0300

    Login Controller added

---
+++ src/com/sifionsolution/riddler/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.sifionsolution.riddler.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class LoginController {
+
+	@Get("/entrar")
+	public void index() {
+	}
+
+}
