a56027195db1282b32d127ae09727298a53bfcdd
Marco <mtnoronha7@gmail.com>
2014-07-30 11:18:16 -0300

    GenericDAO and HQLParameter classes added

---
+++ src/com/sifionsolution/riddler/dao/GenericDAO.java
@@ -0,0 +1,52 @@
+package com.sifionsolution.riddler.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import com.sifionsolution.riddler.dao.parameters.HqlParameter;
+
+public class GenericDAO<PK, T> {
+
+	private final EntityManager entityManager;
+	private final Class<?> clazz;
+
+	public GenericDAO(Class<?> clazz, EntityManager entityManager) {
+		this.clazz = clazz;
+		this.entityManager = entityManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T getById(PK pk) {
+		return (T) entityManager.find(clazz, pk);
+	}
+
+	public void save(T entity) {
+		entityManager.persist(entity);
+	}
+
+	public void update(T entity) {
+		entityManager.merge(entity);
+	}
+
+	public void delete(T entity) {
+		entityManager.remove(entity);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findAll() {
+		return entityManager.createQuery(("FROM " + clazz.getName())).getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> findByHql(String hql, HqlParameter... params) {
+		Query query = entityManager.createQuery(hql);
+
+		for (HqlParameter param : params)
+			param.apply(query);
+
+		return query.getResultList();
+	}
+
+}
---
+++ src/com/sifionsolution/riddler/dao/parameters/HqlParameter.java
@@ -0,0 +1,19 @@
+package com.sifionsolution.riddler.dao.parameters;
+
+import javax.persistence.Query;
+
+public class HqlParameter {
+	private final String attribute;
+	private final Object value;
+
+	public HqlParameter(String attribute, Object value) {
+		this.attribute = attribute;
+		this.value = value;
+	}
+
+	public void apply(Query query) {
+		if (query != null)
+			query.setParameter(attribute, value);
+	}
+
+}
