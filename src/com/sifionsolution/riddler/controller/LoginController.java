7cc431a0bd8d9d4692e81eed17b97c4fa975e040
Marco <mtnoronha7@gmail.com>
2014-07-31 11:18:41 -0300

    Logging and redirecting

--- src/com/sifionsolution/riddler/controller/LoginController.java
+++ src/com/sifionsolution/riddler/controller/LoginController.java
@@ -2,19 +2,17 @@ package com.sifionsolution.riddler.controller;
 
 import javax.inject.Inject;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresGuest;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.security.annotation.Secured;
 
 import com.sifionsolution.riddler.model.dto.SignInUser;
@@ -26,6 +24,11 @@ public class LoginController {
 	@Inject
 	private Subject currentUser;
 
+	@Inject
+	private Result result;
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequiresGuest
 	@Get("/entrar")
 	public void index() {
@@ -36,11 +39,10 @@ public class LoginController {
 	public void login(SignInUser user) {
 		try {
 			currentUser.login(new UsernamePasswordToken(user.getUsername(), user.getPassword(), false));
-		} catch (UnknownAccountException e) {
-		} catch (IncorrectCredentialsException e) {
-		} catch (LockedAccountException e) {
-		} catch (ExcessiveAttemptsException e) {
-		} catch (AuthenticationException e) {
+			result.redirectTo(RootController.class).index();
+		} catch (Exception e) {
+			logger.error("Login attempt unsuccessful", e);
+			result.redirectTo(this).index();
 		}
 	}
 
