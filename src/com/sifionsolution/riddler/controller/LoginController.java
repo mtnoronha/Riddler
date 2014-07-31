ea7cea46d942a18564febaefaa37e3217bdf1abe
Marco <mtnoronha7@gmail.com>
2014-07-31 10:59:46 -0300

    Adding authorization rules through annotations

--- src/com/sifionsolution/riddler/controller/LoginController.java
+++ src/com/sifionsolution/riddler/controller/LoginController.java
@@ -8,24 +8,30 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresGuest;
 import org.apache.shiro.subject.Subject;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.security.annotation.Secured;
 
 import com.sifionsolution.riddler.model.dto.SignInUser;
 
 @Controller
+@Secured
 public class LoginController {
 
 	@Inject
 	private Subject currentUser;
 
+	@RequiresGuest
 	@Get("/entrar")
 	public void index() {
 	}
 
+	@RequiresGuest
 	@Post("/login")
 	public void login(SignInUser user) {
 		try {
@@ -38,6 +44,7 @@ public class LoginController {
 		}
 	}
 
+	@RequiresAuthentication
 	@Get("/logout")
 	public void logout() {
 		currentUser.logout();
