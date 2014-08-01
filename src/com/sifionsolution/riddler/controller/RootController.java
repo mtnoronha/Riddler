3b73a6608a02a085f9f068075cc853506790121c
Marco <mtnoronha7@gmail.com>
2014-08-01 15:48:52 -0300

    Injecting Subject on RootController

--- src/com/sifionsolution/riddler/controller/RootController.java
+++ src/com/sifionsolution/riddler/controller/RootController.java
@@ -1,10 +1,17 @@
 package com.sifionsolution.riddler.controller;
 
+import javax.inject.Inject;
+
+import org.apache.shiro.subject.Subject;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 
 @Controller
 public class RootController {
+
+	@Inject
+	private Subject currentUser;
 
 	@Path("/")
 	public void index() {
