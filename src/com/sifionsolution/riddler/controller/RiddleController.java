75bbecea86699444452ca856b5aff7bb5bad3079
Marco <mtnoronha7@gmail.com>
2014-08-05 10:02:42 -0300

    Edit and List features implemented.

--- src/com/sifionsolution/riddler/controller/RiddleController.java
+++ src/com/sifionsolution/riddler/controller/RiddleController.java
@@ -9,12 +9,16 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.validator.Validator;
 
+import com.sifionsolution.riddler.model.Riddle;
 import com.sifionsolution.riddler.model.dao.RiddleDAO;
 import com.sifionsolution.riddler.model.dto.SaveableClue;
 import com.sifionsolution.riddler.model.dto.SaveableRiddle;
@@ -33,16 +37,41 @@ public class RiddleController {
 	@Inject
 	private Result result;
 
-	@Get("/enigma")
-	public void index() {
+	private static final Logger logger = LoggerFactory.getLogger(RiddleController.class);
+
+	@Get({ "/enigma/{riddle}", "/enigma", "/enigma/" })
+	public void index(Riddle riddle) {
+		result.include("riddle", riddle);
+
+		if (riddle != null) {
+			result.include("clues", riddle.getClues());
+		}
+	}
+
+	@Get("/enigmas")
+	public void list() {
+		result.include("riddles", dao.list());
 	}
 
 	@Post("/enigma/salvar")
 	public void save(@NotNull @Valid SaveableRiddle riddle, @NotNull @Valid List<SaveableClue> clues) {
-		validator.onErrorRedirectTo(RiddleController.class).index();
+		Riddle entity = new Riddle();
+		entity.load(riddle, clues);
+
+		validator.onErrorRedirectTo(RiddleController.class).index(entity);
 
 		dao.save(riddle, clues);
 
-		result.redirectTo(RiddleController.class).index();
+		result.redirectTo(RiddleController.class).index(null);
+	}
+
+	// TODO implement this later
+	@Get("/enigma/remove/{riddle}")
+	public void remove(Riddle riddle) {
+		if (riddle == null) {
+			logger.info("Cant remove a null Riddle");
+		} else {
+			logger.info("Removing: " + riddle.getId());
+		}
 	}
 }
