19b05174f8275e97a7ca38a848b69fd576c228e8
Marco <mtnoronha7@gmail.com>
2014-08-25 11:33:21 -0300

    Admin can now see list of existing users

---
+++ src/com/sifionsolution/codex/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.sifionsolution.codex.controller;
+
+import static com.sifionsolution.codex.enums.Role.ADMIN;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Result;
+
+import com.sifionsolution.codex.model.dao.UserDAO;
+import com.sifionsolution.codex.model.wrapper.builder.UserWrapperBuilder;
+import com.sifionsolution.codex.security.AllowTo;
+
+@Controller
+@AllowTo(ADMIN)
+public class UserController {
+
+	@Inject
+	private UserDAO dao;
+
+	@Inject
+	private Result result;
+
+	@Inject
+	private UserWrapperBuilder builder;
+
+	@Get("/admin")
+	public void index() {
+		result.include("users", builder.build(dao.listAll()));
+	}
+}
