b4c3bcab125dd8e9e6977642444f0cbc01a61ba3
Marco <mtnoronha7@gmail.com>
2014-08-26 11:53:19 -0300

    AnalysisWrapper that will wrap all information that will be shown on charts

---
+++ src/com/sifionsolution/codex/riddle/test/analysis/AnalysisWrapper.java
@@ -0,0 +1,17 @@
+package com.sifionsolution.codex.riddle.test.analysis;
+
+import com.sifionsolution.codex.google.charts.ChartWrapper;
+
+public class AnalysisWrapper {
+
+	private ChartWrapper overall;
+
+	public AnalysisWrapper(ChartWrapper overall) {
+		this.overall = overall;
+	}
+
+	public ChartWrapper getOverall() {
+		return overall;
+	}
+
+}
