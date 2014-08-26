21e5c45216cd1546d323e62866ad696185c17d7f
Marco <mtnoronha7@gmail.com>
2014-08-26 10:26:22 -0300

    First chart example

--- WebContent/WEB-INF/jsp/analysis/index.jsp
+++ WebContent/WEB-INF/jsp/analysis/index.jsp
@@ -14,8 +14,36 @@
 		
 		<div class="container">
 			<div class="row">
+				<div class="chart" data-url="${linkTo[AnalysisController].test}"></div>
 			</div>
 		</div>
 		<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-	</body>
+		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+		 
+		<script type="text/javascript">
+		    
+		    // Load the Visualization API and the piechart package.
+		    google.load('visualization', '1', {'packages':['corechart']});
+		      
+		    // Set a callback to run when the Google Visualization API is loaded.
+		    google.setOnLoadCallback(drawChart);
+		      
+		    var chartDiv = $('.chart');
+		    function drawChart() {
+		      var jsonData = $.ajax({
+		          url: chartDiv.data('url'),
+		          dataType:"json",
+		          async: false
+		          }).responseText;
+		          
+		      // Create our data table out of JSON data loaded from server.
+		      var data = new google.visualization.DataTable(jsonData);
+		
+		      // Instantiate and draw our chart, passing in some options.
+		      var chart = new google.visualization.PieChart(chartDiv[0]);
+		      chart.draw(data, {width: 400, height: 240});
+		    }
+	
+	    </script>
+  	</body>
 </html>
--- src/com/sifionsolution/codex/controller/AnalysisController.java
+++ src/com/sifionsolution/codex/controller/AnalysisController.java
@@ -7,7 +7,12 @@ import javax.inject.Inject;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
 
+import com.sifionsolution.codex.google.charts.ChartCell;
+import com.sifionsolution.codex.google.charts.ChartColumn;
+import com.sifionsolution.codex.google.charts.ChartWrapper;
+import com.sifionsolution.codex.google.charts.builder.ChartBuilder;
 import com.sifionsolution.codex.security.AllowTo;
 
 @Controller
@@ -19,6 +24,24 @@ public class AnalysisController {
 
 	@Get("/analysis")
 	public void index() {
+	}
+
+	@Get("/analysis/test")
+	public void test() {
+		ChartBuilder builder = new ChartBuilder();
+
+		builder.addColumn(new ChartColumn("", "Topping", "", "string"));
+		builder.addColumn(new ChartColumn("", "Slices", "", "number"));
+
+		builder.addRow(new ChartCell("Mushrooms", null), new ChartCell(new Integer(3), null));
+		builder.addRow(new ChartCell("Onions", null), new ChartCell(new Integer(1), null));
+		builder.addRow(new ChartCell("Olives", null), new ChartCell(new Integer(1), null));
+		builder.addRow(new ChartCell("Zucchini", null), new ChartCell(new Integer(1), null));
+		builder.addRow(new ChartCell("Pepperoni", null), new ChartCell(new Integer(2), null));
+
+		ChartWrapper wrapper = builder.build();
+
+		result.use(Results.json()).withoutRoot().from(wrapper).recursive().serialize();
 	}
 
 }
